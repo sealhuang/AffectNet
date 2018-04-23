36b09fe0dc9ee15a9640029b4f4ffca874ce21ac
Ankit <ankit.sharma285@gmail.com>
2018-04-22 23:11:12 -0400

    code

--- data_augmentation.py
+++ data_augmentation.py
@@ -14,10 +14,11 @@ count = 1
 img_dir = '../data_affect/train/'
 
 while(count < no_images):
-    filename = img_dir + '/image' + str(image_id).zfill(7) + '.jpg'
+    filename = img_dir + '/image' + str(count).zfill(7) + '.jpg'
     img = load_img(filename)
     x = img_to_array(img)
     x = x.reshape((1,) + x.shape)
+    count = count + 1
     i = 0
     for batch in datagen.flow(x, y=1,batch_size=1, save_to_dir='data_augment', save_prefix='image', save_format='jpeg'):
         i += 1
