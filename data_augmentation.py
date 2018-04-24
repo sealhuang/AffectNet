c7a9664f9a1e564b32040f89969dd43f7698ce07
Ankit <ankit.sharma285@gmail.com>
2018-04-24 18:19:47 -0400

    22k

--- data_augmentation.py
+++ data_augmentation.py
@@ -17,9 +17,9 @@ datagen = ImageDataGenerator(
         fill_mode='nearest')
 
 nb_train_samples = 42553
-no_images = 22000
-img_count = 17001
-dst_img_count = 101978
+no_images = 27000
+img_count = 22001
+dst_img_count = 131971
 img_dir = '../data_affect/train'
 no_aug_images = 6
 aug_images_list = []
