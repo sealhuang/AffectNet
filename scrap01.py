d21bfd613997281b1f840de98ce0d7770c58ea58
Ankit <ankit.sharma285@gmail.com>
2018-04-26 14:53:32 -0400

    s

--- scrap01.py
+++ scrap01.py
@@ -1,64 +1,83 @@
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import os, h5py
+import scipy.io as mat_load
 import numpy as np
-from os import listdir,remove
-import shutil
-import os
+np.random.seed(1337)  # for reproducibility
 
-parent_folder = 'buffer/'
-destination_folder = 'data_augment/'
+import numpy.random as rng
+from keras.models import Sequential, Model, model_from_json, load_model
+from keras.layers import Dense, Dropout, Input, Lambda
+from keras import optimizers
+from keras import backend as K
+from keras.layers import Conv2D, MaxPooling2D,concatenate
+from keras.layers import Activation, Dropout, Flatten, Dense,Input
+from keras.utils.data_utils import get_file
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from keras.preprocessing import  image
+from keras.applications.vgg16 import preprocess_input
+from keras.applications import vgg16, vgg19,inception_v3
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import ModelCheckpoint
 
-datagen = ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
+# TODO: check on the size of input images MOON paper
+img_width, img_height, depth = 224,224,3
+img_input = Input(shape=(img_height,img_width,depth))
+nb_train_samples = 255250
+#nb_train_samples = 1000
+nb_validation_samples = 4500
+#nb_validation_samples = 1200
+nb_epoch = 50
+batch_size = 64
 
-nb_train_samples = 42553
-no_images = 430
-img_count = 428
-dst_img_count = 1
-img_dir = '../data_affect/train'
-no_aug_images = 6
-aug_images_list = []
+#Step 1: Train generator
+def train_generator():
+    #full_image_dir = 'keras_data/full_image/train'
+    img_dir = '../augmented_images'
+    label_file = mat_load.loadmat('da_train_labels_5000')
+    label_data = label_file['da_train_labels']
+    #landmark_data = label_file['landmark_labels']
+    train_batch_size = batch_size
+    image_id = 1
+    train_index_thr = batch_size * int(nb_train_samples/batch_size)
 
-while(img_count <= no_images):
-    filename = img_dir + '/image' + str(img_count).zfill(7) + '.jpg'
-    print(filename)
-    prefix_str = 'image_' + str(img_count)
-    img = load_img(filename)
-    x = img_to_array(img)
-    x1 = []
-    x1 = x.reshape((1,) + x.shape)
-    img_count = img_count + 1
+    while True:
+        batch_labels = []
+        batch_landmark_labels = []
+        batch_feature = []
+        image_count = 0
 
-    i = 0
-    os.mkdir(parent_folder)
-    for x_batch in datagen.flow(x1,batch_size=1, save_to_dir=parent_folder, save_prefix= prefix_str, save_format='jpeg'):
-        i+= 1
-        if i > (no_aug_images-1):
-            break
+        if((image_id+batch_size) > train_index_thr):
+            image_id = 1
 
-    files = listdir(parent_folder)
-    files_count = 1
 
-    for name in files:
-        dst_filename = destination_folder + 'image' + str(dst_img_count).zfill(7) + '.jpeg'
-        print(dst_filename)
-        src_filename = parent_folder + str(name)
-        shutil.copyfile(src_filename,dst_filename)
-        remove(src_filename)
-        dst_img_count = dst_img_count + 1
-        files_count = files_count + 1
+        while(image_count < batch_size and image_id < nb_train_samples):
+            try:
+                filename = img_dir + '/image' + str(image_id).zfill(7) + '.jpg'
+                body_img = image.load_img(filename, target_size=(img_height,img_width))
+                x = image.img_to_array(body_img)
+                x = np.expand_dims(x, axis=0)
+                feature01 = preprocess_input(x)
+                img_label = label_data[image_id-1][0]
 
-    aug_images_list += [files_count]
+                landmark_label = label_data[image_id-1][1:]
+                batch_feature += [feature01]
+                batch_labels += [img_label]
+                batch_landmark_labels += [landmark_label]
+                image_count = image_count + 1
+                image_id = image_id + 1
+            except IOError:
+                image_id = image_id + 1
+                continue
 
-    os.rmdir(parent_folder)
+        batch_labels = to_categorical(batch_labels,9)
+        batch_labels = np.array(batch_labels)
+        batch_landmark_labels = np.array(batch_landmark_labels)
+        batch_feature = np.array(batch_feature)
+        batch_feature = np.squeeze(batch_feature)
 
-thefile = open('aug_list.txt', 'w')
-for item in aug_images_list:
-  thefile.write("%s\n" % item)
+        yield (batch_feature,[batch_labels,batch_landmark_labels])
 
-thefile.close()
+[feature01,[labels01,c_labels02]] = next(train_generator())
+print(feature01.shape)
+print(labels01.shape)
+print(c_labels02.shape)
